6edda76123090b07339bdd5422cb3531a2d5eea6
Vimal Kumar Thanikachalam <vimaleurakaa@gmail.com>
2020-11-12 14:48:56 +0530

    init

---
+++ js/footer.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+  document.getElementById("footer").innerHTML += `
+<footer>
+<div class="container container-custom">
+<div class="row">
+<div class="col-sm-12 col-md-12 col-lg-4">
+<div class="foot-contact-block">
+<img src="images/Balance-Plus-Logo-white.png" class="img-fluid" alt="#">
+<p>
+Lorem ipsum dolor sit amet, consect <br> etur adipisicing elit, sed do eius mod <br>
+tempor incididunt ut labore et dolore<br> magna aliqua. Ut enim ad minim
+</p>
+<a href="tel:91 123456789">
+<h4><i class="fas fa-phone"></i>+91 123456789</h4>
+</a>
+<a href="">
+<h4><i class="far fa-envelope"></i><span>info@balanceplus.in</span></h4>
+</a>
+</div>
+</div>
+<div class="col-sm-6 col-md-4 col-lg-2 offset-lg-1">
+<div class="foot-link-box">
+<h4>Quick Links</h4>
+<ul>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>About Us</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Contact Us</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Testimonials</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Blogs</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>WhatsApp</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Payment</a>
+</li>
+</ul>
+</div>
+</div>
+<div class="col-sm-6 col-md-4 col-lg-2">
+<div class="foot-link-box">
+<h4>Our Services</h4>
+<ul>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Physiotherapy</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Dietitian</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Fitness</a>
+</li>
+<li>
+<a href="#"><i class="fas fa-angle-double-right"></i>Corporate Service</a>
+</li>
+
+</ul>
+</div>
+</div>
+<div class="col-md-4 col-lg-2 offset-lg-1">
+<div class="foot-link-box footlink-box_btn">
+<!-- <a href="#" class="btn btn-outline-success">Find a Doctor</a>
+<a href="#" class="btn btn-outline-success">Career</a>
+<a href="#" class="btn btn-outline-success">Newsletter</a> -->
+<ul>
+<li>
+<a href="#"><i class="fab fa-facebook-f"></i></a>
+</li>
+<li>
+<a href="#"><i class="fab fa-twitter"></i></a>
+</li>
+<li>
+<a href="#"><i class="fab fa-instagram"></i></a>
+</li>
+<li>
+<a href="#"><i class="fab fa-linkedin"></i></a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<div class="copyright">
+ <p>© BalancePlus 2020 Allright Reserved</p>
+<a href="#" id="scroll"><i class="fas fa-angle-double-up"></i></a>
+</div>
+</div>
+</div>
+</div>
+</footer>`;
+});
